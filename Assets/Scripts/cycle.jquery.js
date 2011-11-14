9981845371d1c718964f36af737c9895919d6517
Bradley Few <few.bradley@gmail.com>
2011-11-14 17:08:38 +0000

    Updated readme

---
+++ Assets/Scripts/cycle.jquery.js
@@ -0,0 +1,89 @@
+/* Cycle.jQuery.js
+ **********************************************************************/
+
+
+
+
+(function($){
+	
+	$.fn.cycle = function() {
+		
+	var options = {
+		
+		};
+		
+		
+	var container = document.getElementById('cycle'),
+		lis = container.getElementsByTagName('li'),
+		len = lis.length,
+		arr = [],
+		stopper = false;
+	
+	// Determing how many banners we want visible at a given time is simple.
+	var count = 4, // Determine how many banners should be visible
+		marginBtm = 20; // Your li margin bottom. We need this because we set paddingTop to li height + marginTop
+	
+	// If count is set to be more or equal to the length of our elements then return the function
+	// as there is no point in doing anything.
+	if (count >= len) {
+		stopper = true;
+		return false;
+	}
+	
+	// Hide the banners that shouldn't be visible
+	while(len--) {
+		if (len > count-1) {
+			lis[len].className = 'hide';
+		}
+	}
+	
+	function run() {
+		var firstElem = lis[0],
+			len = lis.length,
+			cloned;
+		
+		// Add exact padding to the container, so nothing jumps
+		container.style.paddingTop = firstElem.clientHeight + 20 +'px';
+		// Apply class as it's more effecient. This class sets; position:absolute; top:0;
+		firstElem.className = 'firstElem';
+		
+		cloned = firstElem.cloneNode(true);
+		
+		
+		
+		// Pull in the first hidden element
+		
+		lis[count].className = 'show';
+		jQuery(lis[count]).animate({opacity:1});
+		
+		// Above we are added a show class to the next element we bring in,
+		// Now we must remove that class by tracking back 1 and setting it to blank
+		// It was conflicting with the first class we set to take our first element out
+		// of the page!
+		lis[count-1].className = '';
+		
+	
+		// Animate the paddingTop of our container
+		jQuery(container).animate({paddingTop:0}, 400);
+		// Animate the opacity of our element
+		
+		jQuery(firstElem).animate({opacity:0}, 1000, function() {
+			// Remove our element
+			container.removeChild(firstElem);
+			container.appendChild(cloned);
+			lis[len-1].className = 'hide';
+		});
+		
+	}
+	
+	// This runs only if our count is less than the lis length
+	if (!stopper) {
+		var timer = window.setInterval(run, 3000);
+	}
+	
+		
+		
+		
+	};
+	
+})(jQuery);
---
+++ README.md
@@ -0,0 +1,8 @@
+<h1>Banner Cycle</h1>
+<p><strong></strong></p>
+<p>Banner cycle loops through a list of elements and rotates the ones to be shown and hidden. It is cross browser - <strong>even ie6:|</strong></p>
+<p>It accepts an integer, checks it against the length of the elements and then applies the relevant classes to the ones that need to be hidden. We then loop through the list of elements, cloning the first one and then removing it and appending it to the end.</p>
+<p>It's made visually cuter by jQuery:)</p>
+<h2>What the hell is next?</h2>
+<p>I'm going to make this into a jQuery plugin.</p>
+<p><i>Thanks:)</i></p>
